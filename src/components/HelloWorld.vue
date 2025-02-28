2b50977d2105845410040d8d531a7afade53d8cb
Tristan Bridge <tbridge360@gmail.com>
2025-02-28 14:31:11 +0100

    remove page turning functionality

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,9 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import Flipbook from 'flipbook-vue'
-
-const pages = ref([])
-
-onMounted(() => {
-  for (let i = 1; i <= 20; i++) {
-    const imageName = i.toString().padStart(2, '0')
-    const path = `/pdf-images/a8alta.pdf 15-10-51-478_Page_${imageName}.png`
-    pages.value.push(path)
-  }
-})
 </script>
 
 <template>
-  <div class="flipbook-container">
-    <Flipbook :pages="pages" class="flipbook"/>
-  </div>
+  hi
 </template>
 
 <style scoped>
-.flipbook-container {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.flipbook {
-  width: 90vw; 
-  height: 90vh;
-  border: 1px solid #ccc;
-}
 </style>
