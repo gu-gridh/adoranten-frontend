63eb11b6b6f42538685976fe18884c98788a554d
Tristan Bridge <tbridge360@gmail.com>
2025-04-29 14:55:04 +0200

    updated footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,28 @@
-<script setup>
-</script>
-
 <template>
   <footer id="footer">
-    <div id="info">
-        <a href="https://www.gu.se/digital-humaniora" target="_blank">GRIDH Home</a>
-        <a href="https://github.com/gu-gridh/" target="_blank">GitHub repository</a>
-        <div>Address: Box 100, 405 30 Gothenburg</div>
-        <div>Telephone: +46 (0) 31-786 0000</div>
+    <div id="developer-info">
+      <div class="links-stack">
+        <a class="site-link link"
+          href=""
+          target="_blank">GitHub
+          repositories and development</a>
+        <a class="site-link link" href=""
+          target="_blank">Database and API documentation</a>
       </div>
-    <div>
-      <a href="https://dh.gu.se/">DH</a>
-      <a href="https://www.gu.se/">GU</a>
+      <!-- <div class="links-stack border-style">
+        <a class="site-link link" href=""
+          target="_blank">Dataset documentation</a>
+      </div> -->
+      <div class="links-stack border-style extra">
+        <a class="site-link link" href=""
+          target="_blank">Project team</a>
+        <a class="site-link link" href="" target="_blank">Feedback form</a>
+      </div>
+    </div>
+    <div class="partners">
+      <a href="https://dh.gu.se/">
+        <div class="link GRIDHLogo" id="">GU / GRIDH</div>
+      </a>
     </div>
   </footer>
 </template>
@@ -29,12 +40,90 @@
   text-align: left;
 }
 
-#info {
+#developer-info {
   display: flex;
-  flex-direction: column;
+  align-items: flex-end;
+  margin-left: 35px;
+  margin-top: 15px;
+  font-size: 1.5em;
+  line-height: 1.4;
+  font-weight: 200;
 }
 
-#info > div {
+.links-stack {
+  margin-right: 20px;
+}
+
+.links-stack a {
+  display: block;
+}
+
+.border-style {
+  border-width: 0 0 0 0.5px;
+  border-color: white;
+  border-style: dashed;
+  padding-left: 20px;
+  padding-right: 10px;
+}
+
+.download-section a {
+  display: inline;
+}
+
+.download-section {
+  display: inline-flex;
+  align-items: center;
+}
+
+.download-link {
+  background: url("https://data.dh.gu.se/ui-icons/download_white_circle.png");
+  background-size: 18px;
+  background-position: 0px 50%;
+  background-repeat: no-repeat;
+  padding-left: 25px;
+  margin-left: 0px;
+}
+
+.site-link {
+  background: url("https://data.dh.gu.se/ui-icons/arrow_link_white_circle.png");
+  background-size: 18px;
+  background-position: 0px 50%;
+  background-repeat: no-repeat;
+  padding-left: 25px;
+  font-size: 18px;
+}
+
+.partners {
+  margin-left: auto;
+}
+
+.link {
+  color: white;
+}
+
+.link:visited {
+  color: white;
+}
+
+.link:hover {
+  opacity: 0.8;
+}
+
+.GRIDHLogo {
+  float: right;
+  margin-top: 30px;
+  width: auto;
+  height: auto;
+  text-align: left;
+  font-weight: 100;
+  font-style: normal;
+  font-size: 30px;
+  line-height: 0.8;
+  padding: 0px 40px 5px 30px;
+  vertical-align: middle;
+}
+
+#info>div {
   margin-top: 8px;
 }
 
